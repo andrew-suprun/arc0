e6109ae223634665bc2524f0598252b4ff63909c
Andrew Suprun <andrew.suprun@gmail.com>
2023-02-17 18:35:05 -0500

    wip: scanning source

--- archiver/main.go
+++ archiver/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"os"
+	"strings"
+	"sync/atomic"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -18,16 +18,182 @@ import (
 	"scanner/lifecycle"
 )
 
-func main() {
-	lc := lifecycle.New()
+type archiver struct {
+	*lifecycle.Lifecycle
+	fyne.App
+	fyne.Window
+	ch    chan any
+	cards *fyne.Container
+	data  chan *state
+}
 
-	a := app.New()
+type state struct {
+	source  scanUI
+	targets []scanUI
+}
+
+type scanUI struct {
+	path  string
+	start time.Time
+	form  *fyne.Container
+}
+
+func newArchiver() *archiver {
+	lc := lifecycle.New()
+	a := app.NewWithID("archiver")
+	ch := make(chan any)
 	a.Lifecycle().SetOnStopped(func() {
 		lc.Stop()
 	})
-	w := a.NewWindow("List Widget")
-	w.Resize(fyne.NewSize(4000, 3000))
+	w := a.NewWindow("Archiver")
+	w.Resize(fyne.NewSize(4000, 750))
+	cards := container.NewVBox()
+	border := container.NewBorder(nil, nil, nil, nil, cards)
+	w.SetContent(border)
+	data := make(chan *state, 1)
+	data <- &state{}
 
+	return &archiver{
+		Lifecycle: lc,
+		App:       a,
+		Window:    w,
+		ch:        ch,
+		cards:     cards,
+		data:      data,
+	}
+}
+
+func main() {
+	app := newArchiver()
+	go app.run()
+	app.ch <- selectExistingSourceMsg{}
+	app.ShowAndRun()
+}
+
+func (app *archiver) run() {
+	for msg := range app.ch {
+		go app.handleMsg(msg)
+	}
+}
+
+type selectExistingSourceMsg struct{}
+type selectNewSourceMsg struct{}
+type selectTargetsMsg struct{}
+type hashPathMsg struct {
+	path string
+}
+
+func (app *archiver) handleMsg(msg any) {
+	switch msg := msg.(type) {
+	case selectExistingSourceMsg:
+		app.selectExistingSource()
+	case selectNewSourceMsg:
+		app.selectNewSource()
+	case selectTargetsMsg:
+		app.selectTargets()
+	case hashPathMsg:
+		app.hashPath(msg.path)
+	case fs.ScanStat:
+		app.scanStat(msg)
+	case fs.ScanFileResult:
+		app.scanFileInfo(msg)
+	default:
+		log.Panicf("Unhandled msg: %#v\n", msg)
+	}
+}
+
+func (app *archiver) selectExistingSource() {
+	sourcesStr := app.Preferences().String("sources")
+	fmt.Println(sourcesStr)
+	if sourcesStr != "" {
+		var done atomic.Bool
+		sourceBtns := container.NewVBox()
+		d := dialog.NewCustom("Select Source", "Cancel", sourceBtns, app.Window)
+		sources := strings.Split(sourcesStr, "|")
+		for _, source := range sources {
+			btn := widget.NewButton(source, func() {
+				fmt.Println("### 1")
+				form := scanForm()
+				data := <-app.data
+				data.source = scanUI{
+					path: source,
+					form: form,
+				}
+				app.data <- data
+				card := widget.NewCard(source, "", form)
+				app.cards.Add(card)
+				app.ch <- hashPathMsg{path: source}
+				app.ch <- selectTargetsMsg{}
+				done.Store(true)
+				d.Hide()
+			})
+			btn.Importance = widget.HighImportance
+			sourceBtns.Add(btn)
+		}
+		anotherSourceBtn := widget.NewButton("Select Another Source", func() {
+			fmt.Println("### 2")
+			app.ch <- selectNewSourceMsg{}
+			done.Store(true)
+			d.Hide()
+		})
+		anotherSourceBtn.Importance = widget.HighImportance
+		sourceBtns.Add(anotherSourceBtn)
+		d.SetOnClosed(func() {
+			if !done.Load() {
+				fmt.Println("### 3")
+				app.ch <- selectNewSourceMsg{}
+			}
+		})
+		d.Show()
+	} else {
+		fmt.Println("### 4")
+		app.ch <- selectNewSourceMsg{}
+	}
+}
+
+func (app *archiver) selectNewSource() {
+	dialog.ShowFolderOpen(func(url fyne.ListableURI, err error) {
+		if url == nil {
+			app.Quit()
+		}
+
+		path := url.Path()
+		sourcesStr := app.Preferences().String("sources")
+		sources := strings.Split(sourcesStr, "|")
+		if sourcesStr == "" {
+			app.Preferences().SetString("sources", path)
+		} else {
+			found := false
+			for _, source := range sources {
+				if path == source {
+					found = true
+					break
+				}
+			}
+			if !found {
+				app.Preferences().SetString("sources", sourcesStr+"|"+path)
+			}
+		}
+
+		form := scanForm()
+		data := <-app.data
+		data.source = scanUI{
+			path: path,
+			form: form,
+		}
+		app.data <- data
+		card := widget.NewCard(path, "", form)
+		app.cards.Add(card)
+		app.ch <- hashPathMsg{path: path}
+		app.ch <- selectTargetsMsg{}
+	}, app.Window)
+}
+
+func (app *archiver) selectTargets() {
+	fmt.Println("select targets")
+}
+
+func scanForm() *fyne.Container {
 	fileProgress := widget.NewProgressBar()
 	fileProgress.Min = 0.0
 	fileProgress.Max = 100.0
@@ -49,63 +215,60 @@ func main() {
 		widget.NewProgressBarInfinite(),
 	)
 
-	card := widget.NewCard("", "", form)
-
-	border := container.NewBorder(nil, nil, nil, nil, card)
-
-	dialog.ShowFolderOpen(func(url fyne.ListableURI, err error) {
-		fmt.Println(url, err)
-		if url == nil {
-			os.Exit(0)
-		}
-		card.Title = "Scanning " + url.Path()
-		card.Refresh()
-		w.SetContent(border)
-		go hash(lc, form, url.Path())
-	}, w)
-	w.ShowAndRun()
+	return form
 }
 
-func hash(lc *lifecycle.Lifecycle, form *fyne.Container, path string) {
-	results := fs.Scan(lc, path)
-	var start time.Time
-	var nilTime time.Time
-	for result := range results {
-		if start == nilTime {
-			overallProgress := widget.NewProgressBar()
-			overallProgress.Min = 0.0
-			overallProgress.Max = 100.0
-			overallProgress.TextFormatter = func() string {
-				return fmt.Sprintf("%.1f%%", overallProgress.Value)
-			}
-			form.Objects[9] = overallProgress
+var nilTime time.Time
 
-			start = time.Now()
-		}
-		switch update := result.(type) {
-		case fs.ScanFileResult:
-		case fs.ScanStat:
-			fileProgress := float64(update.Hashed) / float64(update.Size)
-			etaProgress := float64(update.TotalHashed) / float64(update.TotalToHash)
-			overallHashed := update.TotalSize - update.TotalToHash + update.TotalHashed
-			overalProgress := float64(overallHashed) / float64(update.TotalSize)
-			dur := time.Since(start)
-			eta := start.Add(time.Duration(float64(dur) / etaProgress))
-			remainig := time.Until(eta)
-			form.Objects[1].(*widget.Label).Text = update.Path
-			form.Objects[3].(*widget.Label).Text = eta.Format(time.TimeOnly)
-			form.Objects[5].(*widget.Label).Text = remainig.Truncate(time.Second).String()
-			form.Objects[7].(*widget.ProgressBar).Value = fileProgress * 100
-			if pb, ok := form.Objects[9].(*widget.ProgressBar); ok {
-				pb.Value = overalProgress * 100
-			}
-			form.Refresh()
-
-		case fs.ScanError:
-			log.Printf("stat: file=%s error=%#v, %#v\n", update.Path, update.Error, errors.Unwrap(update.Error))
-		}
+func (app *archiver) scanStat(update fs.ScanStat) {
+	data := <-app.data
+	defer func() {
+		app.data <- data
+	}()
+	var info *scanUI
+	if data.source.path == update.Base {
+		info = &data.source
 	}
-	form.RemoveAll()
-	form.Add(widget.NewLabel("Done"))
-	form.Refresh()
+	if info == nil {
+		log.Panicf("Cannot find scan info for %v\n", update.Path)
+	}
+	if info.start == nilTime {
+		overallProgress := widget.NewProgressBar()
+		overallProgress.Min = 0.0
+		overallProgress.Max = 100.0
+		overallProgress.TextFormatter = func() string {
+			return fmt.Sprintf("%.1f%%", overallProgress.Value)
+		}
+		info.form.Objects[9] = overallProgress
+
+		info.start = time.Now()
+	}
+
+	fileProgress := float64(update.Hashed) / float64(update.Size)
+	etaProgress := float64(update.TotalHashed) / float64(update.TotalToHash)
+	overallHashed := update.TotalSize - update.TotalToHash + update.TotalHashed
+	overalProgress := float64(overallHashed) / float64(update.TotalSize)
+	dur := time.Since(info.start)
+	eta := info.start.Add(time.Duration(float64(dur) / etaProgress))
+	remainig := time.Until(eta)
+	info.form.Objects[1].(*widget.Label).Text = update.Path
+	info.form.Objects[3].(*widget.Label).Text = eta.Format(time.TimeOnly)
+	info.form.Objects[5].(*widget.Label).Text = remainig.Truncate(time.Second).String()
+	info.form.Objects[7].(*widget.ProgressBar).Value = fileProgress * 100
+	if pb, ok := info.form.Objects[9].(*widget.ProgressBar); ok {
+		pb.Value = overalProgress * 100
+	}
+	info.form.Refresh()
+}
+
+func (app *archiver) scanFileInfo(info fs.ScanFileResult) {
+	fmt.Printf("file %v\n", info.Path)
+}
+
+func (app *archiver) hashPath(path string) {
+	fs.Scan(app.Lifecycle, path, app.ch)
+
+	// form.RemoveAll()
+	// form.Add(widget.NewLabel("Done"))
+	// form.Refresh()
 }
--- fs/scanner.go
+++ fs/scanner.go
@@ -21,6 +21,7 @@ import (
 type ScanFileResult *meta.FileMeta
 
 type ScanStat struct {
+	Base        string
 	Path        string
 	Size        int
 	Hashed      int
@@ -34,126 +35,120 @@ type ScanError struct {
 	Error error
 }
 
-func Scan(lc *lifecycle.Lifecycle, base string) (results chan any) {
-	results = make(chan any)
+func Scan(lc *lifecycle.Lifecycle, base string, results chan any) {
+	lc.Started()
+	defer lc.Done()
 
-	go func() {
-		lc.Started()
-		defer lc.Done()
+	path, err := filepath.Abs(base)
+	path = norm.NFC.String(path)
+	if err != nil {
+		results <- ScanError{Path: path, Error: err}
+		return
+	}
 
-		path, err := filepath.Abs(base)
-		path = norm.NFC.String(path)
+	fsys := os.DirFS(base)
+
+	infos := collectMeta(lc, fsys, results)
+
+	defer meta.StoreMeta(path, infos)
+
+	inodes := map[uint64]*meta.FileMeta{}
+	for _, meta := range infos {
+		inodes[meta.Ino] = meta
+	}
+
+	storedMetas := meta.ReadMeta(path)
+
+	for _, storedInfo := range storedMetas {
+		if info, ok := inodes[storedInfo.Ino]; ok {
+			if storedInfo.Size == info.Size {
+				info.Hash = storedInfo.Hash
+			}
+		} else {
+			fmt.Println("not found", storedInfo.Path)
+		}
+	}
+
+	var (
+		totalSize       int
+		totalSizeToHash int
+		totalHashed     int
+		hash            = sha256.New()
+		buf             = make([]byte, 4*1024*1024)
+	)
+
+	for _, info := range infos {
+		totalSize += info.Size
+		if info.Hash == "" {
+			totalSizeToHash += info.Size
+		}
+	}
+
+	hashFile := func(info *meta.FileMeta) {
+		hash.Reset()
+
+		file, err := fsys.Open(info.Path)
 		if err != nil {
-			results <- ScanError{Path: path, Error: err}
+			results <- ScanError{Path: info.Path, Error: err}
 			return
 		}
+		defer file.Close()
 
-		fsys := os.DirFS(base)
-		defer close(results)
+		var hashed int
 
-		infos := collectMeta(lc, fsys, results)
-
-		defer meta.StoreMeta(path, infos)
-
-		inodes := map[uint64]*meta.FileMeta{}
-		for _, meta := range infos {
-			inodes[meta.Ino] = meta
-		}
-
-		storedMetas := meta.ReadMeta(path)
-
-		for _, storedInfo := range storedMetas {
-			if info, ok := inodes[storedInfo.Ino]; ok {
-				if storedInfo.Size == info.Size {
-					info.Hash = storedInfo.Hash
-				}
-			} else {
-				fmt.Println("not found", storedInfo.Path)
-			}
-		}
-
-		var (
-			totalSize       int
-			totalSizeToHash int
-			totalHashed     int
-			hash            = sha256.New()
-			buf             = make([]byte, 4*1024*1024)
-		)
-
-		for _, info := range infos {
-			totalSize += info.Size
-			if info.Hash == "" {
-				totalSizeToHash += info.Size
-			}
-		}
-
-		hashFile := func(info *meta.FileMeta) {
-			hash.Reset()
-
-			file, err := fsys.Open(info.Path)
-			if err != nil {
-				results <- ScanError{Path: info.Path, Error: err}
+		for {
+			if lc.ShoudStop() {
 				return
 			}
-			defer file.Close()
 
-			var hashed int
-
-			for {
-				if lc.ShoudStop() {
-					return
-				}
-
-				nr, er := file.Read(buf)
-				if nr > 0 {
-					nw, ew := hash.Write(buf[0:nr])
-					if ew != nil {
-						if err != nil {
-							results <- ScanError{Path: info.Path, Error: err}
-							return
-						}
-					}
-					if nr != nw {
-						results <- ScanError{Path: info.Path, Error: io.ErrShortWrite}
+			nr, er := file.Read(buf)
+			if nr > 0 {
+				nw, ew := hash.Write(buf[0:nr])
+				if ew != nil {
+					if err != nil {
+						results <- ScanError{Path: info.Path, Error: err}
 						return
 					}
 				}
-
-				hashed += nr
-				totalHashed += nr
-
-				if er == io.EOF {
-					break
-				}
-				if er != nil {
-					results <- ScanError{Path: info.Path, Error: err}
+				if nr != nw {
+					results <- ScanError{Path: info.Path, Error: io.ErrShortWrite}
 					return
 				}
-
-				results <- ScanStat{
-					Path:        info.Path,
-					Size:        info.Size,
-					Hashed:      hashed,
-					TotalSize:   totalSize,
-					TotalToHash: totalSizeToHash,
-					TotalHashed: totalHashed,
-				}
 			}
-			info.Hash = base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
-			results <- ScanFileResult(info)
-		}
 
-		for _, info := range infos {
-			if info.Hash == "" {
-				if lc.ShoudStop() {
-					return
-				}
-				hashFile(info)
+			hashed += nr
+			totalHashed += nr
+
+			if er == io.EOF {
+				break
+			}
+			if er != nil {
+				results <- ScanError{Path: info.Path, Error: err}
+				return
+			}
+
+			results <- ScanStat{
+				Base:        base,
+				Path:        info.Path,
+				Size:        info.Size,
+				Hashed:      hashed,
+				TotalSize:   totalSize,
+				TotalToHash: totalSizeToHash,
+				TotalHashed: totalHashed,
 			}
 		}
-	}()
+		info.Hash = base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
+		results <- ScanFileResult(info)
+	}
 
-	return results
+	for _, info := range infos {
+		if info.Hash == "" {
+			if lc.ShoudStop() {
+				return
+			}
+			hashFile(info)
+		}
+	}
 }
 
 func collectMeta(lc *lifecycle.Lifecycle, fsys fs.FS, results chan<- any) (metas meta.FileMetas) {
