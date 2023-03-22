ffb146c00a733e4d38e0283bfa3ce42d28d4ee1e
Andrew Suprun <andrew.suprun@gmail.com>
2023-03-22 15:09:40 -0400

    mock fs: new file generator

--- files/mock_fs/mock_fs.go
+++ files/mock_fs/mock_fs.go
@@ -30,20 +30,11 @@ func (fsys *mockFs) Scan(path string) <-chan any {
 	log.Println("mock scan", path)
 	result := make(chan any)
 	go func() {
-		folder := faker.Sentence()
-		scanFiles := make([]file, rand.Int()%100+20)
-		total_size := 0
-		for i := range scanFiles {
-			if i%10 == 0 {
-				folder = faker.Sentence()
-			}
-			scanFiles[i].name = filepath.Join(folder, faker.Sentence())
-			scanFiles[i].size = rand.Int() % 1000000
-			total_size += scanFiles[i].size
-		}
-
-		total_hashed := 0
+		scanFiles, total_size, total_hashed := genFiles()
 		for _, file := range scanFiles {
+			if file.hash != "" {
+				continue
+			}
 			hashed := 0
 			for hashed < file.size {
 				if total_hashed > total_size {
@@ -85,4 +76,32 @@ func (fsys *mockFs) Scan(path string) <-chan any {
 }
 
 func (fs *mockFs) Stop() {
+}
+
+var nDirs = []int{10, 5, 3, 0}
+var nFiles = []int{40, 20, 10, 5}
+
+func genFiles() ([]file, int, int) {
+	scanFiles := []file{}
+	total_size := 0
+	total_hashed := 0
+	genFilesRec([]string{}, 0, rand.Intn(100), &scanFiles, &total_size, &total_hashed)
+	return scanFiles, total_size, total_hashed
+}
+
+func genFilesRec(dirs []string, level, pcHashed int, files *[]file, total_size, total_hashed *int) {
+	path := filepath.Join(dirs...)
+	for i := 0; i < nFiles[level]; i++ {
+		size := rand.Int() % 100000
+		*total_size += size
+		hash := ""
+		if rand.Intn(100) < pcHashed {
+			hash = faker.Phonenumber()
+			*total_hashed += size
+		}
+		*files = append(*files, file{name: filepath.Join(path, faker.Sentence()), size: size, hash: hash})
+	}
+	for i := 0; i < nDirs[level]; i++ {
+		genFilesRec(append(dirs, faker.Sentence()), level+1, pcHashed, files, total_size, total_hashed)
+	}
 }
